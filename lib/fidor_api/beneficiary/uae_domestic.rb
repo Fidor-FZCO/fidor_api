83cb5e1618e665ab5d6db1ffd6e05521448be5fb
Konstantin Munteanu <munteanu@fidor.de>
2016-09-29 10:19:30 +0200

    Refactoring FidorApiClient - Will behave more like ActiveRecord - ApiClient boilerplate will become obsolete

--- lib/fidor_api/beneficiary/uae_domestic.rb
+++ lib/fidor_api/beneficiary/uae_domestic.rb
@@ -10,14 +10,10 @@ module FidorApi
       validates :account_number, presence: true
       validates :swift_code,     presence: true
 
-      def initialize(attrs = {})
+      def set_attributes(attrs = {})
         self.account_number = attrs.fetch("routing_info", {})["account_number"]
         self.swift_code     = attrs.fetch("routing_info", {})["swift_code"]
         super(attrs.except("routing_type", "routing_info"))
-      end
-
-      def save
-        create
       end
 
       def as_json_routing_type
@@ -33,13 +29,9 @@ module FidorApi
 
       private
 
-      def self.resource
-        "beneficiaries"
-      end
-
       module ClientSupport
         def build_uae_domestic_beneficiary(attributes = {})
-          Beneficiary::UaeDomestic.new(attributes.merge(client: self))
+          Beneficiary::UaeDomestic.new(attributes)
         end
       end
     end
--- lib/fidor_api/transfer/pending.rb
+++ lib/fidor_api/transfer/pending.rb
@@ -1,15 +1,13 @@
 module FidorApi
   module Transfer
-    class Pending < Resource
+    class Pending < Connectivity::Resource
       include Generic
 
-      def initialize(attrs = {})
+      self.endpoint = Connectivity::Endpoint.new('/pending_transfers', :collection)
+
+      def set_attributes(attrs = {})
         set_beneficiary_attributes(attrs)
         super(attrs.except("beneficiary"))
-      end
-
-      def self.find(id)
-        new(request(access_token: nil, endpoint: "/pending_transfers/#{id}", htauth: true).body)
       end
 
       module ClientSupport
