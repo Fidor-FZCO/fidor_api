0e31c9102f769c419da4d4533652afc62fe99541
Christian Seiler <chr.seiler@gmail.com>
2018-06-29 16:07:41 +0200

    add generic FPS transfers

---
+++ lib/fidor_api/transfer/legacy/fps.rb
@@ -0,0 +1,58 @@
+module FidorApi
+  module Transfer
+    module Legacy
+      class Fps < Base
+        extend ModelAttribute
+        extend AmountAttributes
+
+        self.endpoint = Connectivity::Endpoint.new('/fps_transfers', :collection)
+
+        attribute :id,               :integer
+        attribute :account_id,       :string
+        attribute :user_id,          :string
+        attribute :transaction_id,   :string
+        attribute :remote_account,   :string
+        attribute :remote_sort_code, :string
+        attribute :remote_name,      :string
+        attribute :external_uid,     :string
+        attribute :subject,          :string
+        attribute :currency,         :string
+        attribute :subject,          :string
+        attribute :state,            :string
+        attribute :created_at,       :time
+        attribute :updated_at,       :time
+        amount_attribute :amount
+
+        def self.required_attributes
+          [ :account_id, :external_uid, :remote_account, :remote_sort_code, :remote_name, :amount, :subject ]
+        end
+
+        def self.writeable_attributes
+          required_attributes
+        end
+
+        validates *required_attributes, presence: true
+
+        def as_json
+          attributes.slice *self.class.writeable_attributes
+        end
+
+        private
+
+        module ClientSupport
+          def fps_transfers(options = {})
+            Transfer::Legacy::Fps.all(options)
+          end
+
+          def fps_transfer(id)
+            Transfer::Legacy::Fps.find(id)
+          end
+
+          def build_fps_transfer(attributes = {})
+            Transfer::Legacy::Fps.new(attributes)
+          end
+        end
+      end
+    end
+  end
+end
